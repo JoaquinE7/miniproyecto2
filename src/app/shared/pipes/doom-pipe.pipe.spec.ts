c7c0fd1523ade3bb38cffe735639ee4ecc8464f7
Gerardo Castro <104610703+LittleWea@users.noreply.github.com>
2023-05-07 23:22:46 -0600

    Delete src directory

--- src/app/shared/pipes/doom-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DoomPipePipe } from './doom-pipe.pipe';
-
-describe('DoomPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DoomPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
